ff076e1a9074617366fe00ba4e21c3e64d7f4710
Hari <haridurgaprasad23@gmail.com>
2024-01-07 14:46:55 +0530

    variable commit

--- dart1.dart
+++ dart1.dart
@@ -1,3 +1,7 @@
 void main(){
     print('hello');
+    print("this is my first code");
+    print(12/3);
+    print('1=99');
+
 }
---
+++ variables.dart
@@ -0,0 +1,17 @@
+void main(){
+    //variables
+    //Numbers-int,double
+    int score =24;
+     double num=99.1;
+
+    //Strings
+    String name= 'del';
+
+    //booleans
+    bool isALive = true;
+    print(score);
+    print(num);
+    print(name);
+    print(isALive);
+
+}
