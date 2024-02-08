10f73b1facd360b090a66615c039408a38484274
Hari <haridurgaprasad23@gmail.com>
2024-02-09 03:28:06 +0530

    lambda

---
+++ lambda.dart
@@ -0,0 +1,24 @@
+void main(){
+  Function addTwoNumbers= (int a, int b){
+                          var sum = a+b;
+                          print(sum);
+  };
+  var multipleByFour = (int number){
+    return number*4;
+
+  };
+    
+  Function addNumbers= (int a, int b){
+                        
+                          print(a+b);
+  
+  };
+  addTwoNumbers(2,5);
+  print(multipleByFour(5));
+}
+
+
+void addMyNumbers(int a,int b){
+  var sum = a + b;
+  print(sum);
+}
