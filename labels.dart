4664304c45a6dae14603f0d433b8cbeaeafc07ce
Hari <haridurgaprasad23@gmail.com>
2024-02-08 15:30:15 +0530

    labels;

---
+++ labels.dart
@@ -0,0 +1,12 @@
+void main(){
+  myOuterLoop: for(int i=1;i<=3;i++){
+    for(int j=1;j<=4;j++){
+      
+    
+    print("$i $j");
+    if(i==2&&j==2){
+      break myOuterLoop;
+    }
+  }
+  }
+}
