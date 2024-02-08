ad695d660f6b20fed9e337328e4b92c683d69663
Hari <haridurgaprasad23@gmail.com>
2024-02-08 21:00:31 +0530

    catch2

--- try.dart
+++ try.dart
@@ -3,7 +3,8 @@ void main(){
 
     int result =12~/0;
     print("the result is $result");
-  }catch(e){
+  }catch(e,s){
     print("the exception thrown is $e");
+    print("STACK TRACE \n $s");
   }
 }
